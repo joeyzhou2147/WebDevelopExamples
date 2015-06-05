bb709aa66dfb6624ebf2ca5c290bc0daa774dd08
yuzhou <joeyzhou2147@gmail.com>
2015-06-05 11:36:12 +0800

    222

---
+++ WebDev2015-03-11/WebDev2015-03-11/test 1.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="test 1.aspx.cs" Inherits="WebDev2015_03_11.test_1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+    <title>Test 2</title>
+
+    <script type="text/javascript" src="JavaScript1.js"></script>
+
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h2>This is a heading</h2>
+            <p>This is a paragraph.</p>
+            <p>This is another paragraph.</p>
+            <button>Click me</button>
+        </div>
+    </form>
+
+    <script type="text/javascript">
+        $(document).ready(function () {
+            $("button").click(function () {
+                $("p").hide();
+            });
+        });
+    </script>
+</body>
+</html>
---
+++ WebDev2015-03-25/auth.cs
@@ -0,0 +1,39 @@
+﻿namespace WebDev2015_03_25
+{
+    public class auth
+    {
+         <?php
+             if(session_status()==PHP_SESSION_NONE)
+{
+    private session_start();
+}
+
+    include "php/database.connect.php";
+
+    $username = $_REUQEST["u"];// name of input password element, only name left and id class are gone
+    $password = $_REUQEST["p"];// name of input password element
+
+    $statement = $db->prepare("Select aes_decrypt(email,'key') as user, aes_decrypt(password,'key') as pswd FROM users WHERE "
+    +
+    "aes_decrypt(email,'key')=? and aes_decrypt(password,'key')")
+
+    $statement->exexute(array($username,$password));
+    $row = $statement->fetch();
+
+    $user = $row['user'];
+
+    if(isset($user))// success
+{
+    unset($_SESSION['error']);
+    $_SESSION['user'] = $user;
+    require'welcome.php';
+}else()// fail
+{
+    unset($_SESSION['user']);
+    $_SESSION['error'] = "Credentials don't match...";
+    require'index.php';
+}
+exit();
+?>
+    }
+}
